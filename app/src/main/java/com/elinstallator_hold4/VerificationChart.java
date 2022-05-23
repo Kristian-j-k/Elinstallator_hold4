b41117f3e9933b91354d1e7dc3d0e7ea4dfa890e
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2022-05-23 15:50:31 +0200

    teksters tilgængelighed opdateret i klassen verificationChart

--- app/src/main/java/com/elinstallator_hold4/VerificationChart.java
+++ app/src/main/java/com/elinstallator_hold4/VerificationChart.java
@@ -48,48 +48,48 @@ public class VerificationChart {
 
     String text2 = "2. Tavlen:";
 
-    String getQuestion13 = "Er der tilstrækkelig plads til at arbejde på/adgang til tavlen?";
-    String getQuestion14 = "Er overstrømsbeskyttelsesudstyr korrekt valgt og evt. indstillet?";
-    String getQuestion15 = "Er der en entydig mærkning af beskyttelsesudstyr med tilhørsforhold?";
-    String getQuestion16 = "Er der mærkning om max. mærke-/indstillingsstrøm?";
-    String getQuestion17 = "Er mærkning med oplysninger om tekniske data for tavlen foretaget?";
-    String getQuestion18 = "Er udgående beskyttelsesledere anbragt i separate klemmer i tavlen?";
-    String getQuestion19 = "Er afdækning og dækplader monteret?";
-    String getQuestion20 = "Er indføringer tilpasset/tætnet, så tavlens kapslingsklasse er som mærket?";
+    String question13 = "Er der tilstrækkelig plads til at arbejde på/adgang til tavlen?";
+    String question14 = "Er overstrømsbeskyttelsesudstyr korrekt valgt og evt. indstillet?";
+    String question15 = "Er der en entydig mærkning af beskyttelsesudstyr med tilhørsforhold?";
+    String question16 = "Er der mærkning om max. mærke-/indstillingsstrøm?";
+    String question17 = "Er mærkning med oplysninger om tekniske data for tavlen foretaget?";
+    String question18 = "Er udgående beskyttelsesledere anbragt i separate klemmer i tavlen?";
+    String question19 = "Er afdækning og dækplader monteret?";
+    String question20 = "Er indføringer tilpasset/tætnet, så tavlens kapslingsklasse er som mærket?";
 
     //3. Installationen
     String text3 = "3. Installation:";
 
-    String getQuestion3_1 = "Er udstyr til adskillelse og afbrydelse korrekt valgt, placeret og installeret?";
-    String getQuestion3_2 = "Er stikkontakter og udtag m.m. installeret i henhold til gældende bestemmelser?";
-    String getQuestion3_3 = "Er kabler/ledninger korrekt oplagt, afsluttet og forbundet?";
-    String getQuestion3_4 = "Er kabler beskyttet mod mekanisk overlast ved opføringer fra gulv/jord?";
-    String getQuestion3_5 = "Er tilledninger aflastet for træk og vridning ved tilslutning til installationen?";
-    String getQuestion3_6 = "Er alle dæksler og afdækninger monteret, så der ikke er berøringsfare?\n";
-    String getQuestion3_7 = "Er alle samlinger let tilgængelige?";
+    String question21 = "Er udstyr til adskillelse og afbrydelse korrekt valgt, placeret og installeret?";
+    String question22 = "Er stikkontakter og udtag m.m. installeret i henhold til gældende bestemmelser?";
+    String question23 = "Er kabler/ledninger korrekt oplagt, afsluttet og forbundet?";
+    String question24 = "Er kabler beskyttet mod mekanisk overlast ved opføringer fra gulv/jord?";
+    String question25 = "Er tilledninger aflastet for træk og vridning ved tilslutning til installationen?";
+    String question26 = "Er alle dæksler og afdækninger monteret, så der ikke er berøringsfare?\n";
+    String question27 = "Er alle samlinger let tilgængelige?";
 
     //4. Indbygningsamaturer
     String text4 = "4. Indbygningsarmaturer:";
 
-    String getQuestion4_1 = "Er indbygningsarmaturer korrekt valgt og monteret?";
-    String getQuestion4_2 = "Er indbygningsarmaturer installeret således, at overophedning undgås?";
+    String question28 = "Er indbygningsarmaturer korrekt valgt og monteret?";
+    String question29 = "Er indbygningsarmaturer installeret således, at overophedning undgås?";
 
     //5. Beskyttelsesledere
     String text5 = "5. Beskyttelsesledere og udligningsforbindelser:";
 
-    String getQuestion5_1 = "Er jordingslederen korrekt valgt (minimum 6 mm2)?";
-    String getQuestion5_2 = "Er der etableret beskyttende potentialudligning?";
-    String getQuestion5_3 = "Er supplerende beskyttende potentialudligning etableret?";
-    String getQuestion5_4 = "Er den gennemgående forbindelse i udligningsforbindelser kontrolleret?";
-    String getQuestion5_5 = "Er den gennemgående forbindelse i beskyttelsesledere kontrolleret?";
-    String getQuestion5_6 = "Er overgangsmodstand for jordelektroden kontrolleret?";
+    String question30 = "Er jordingslederen korrekt valgt (minimum 6 mm2)?";
+    String question31 = "Er der etableret beskyttende potentialudligning?";
+    String question32 = "Er supplerende beskyttende potentialudligning etableret?";
+    String question33 = "Er den gennemgående forbindelse i udligningsforbindelser kontrolleret?";
+    String question34 = "Er den gennemgående forbindelse i beskyttelsesledere kontrolleret?";
+    String question35 = "Er overgangsmodstand for jordelektroden kontrolleret?";
 
     //6. Fejlbeskyttelse/suplerende beskyttelse
     String text6 = "6. Fejlbeskyttelse/supplerende beskyttelse:";
 
-    String getQuestion6_1 = "Er beskyttelsesmetode korrekt valgt i forhold til installationstype og systemjording?";
-    String getQuestion6_2 = "Er RCD’er (fejlstrømsafbrydere) kontrolleret og afprøvet?";
-    String getQuestion6_3 = "Er klasse I brugsgenstande tilsluttet til beskyttelseslederen?";
+    String question36 = "Er beskyttelsesmetode korrekt valgt i forhold til installationstype og systemjording?";
+    String question37 = "Er RCD’er (fejlstrømsafbrydere) kontrolleret og afprøvet?";
+    String question38 = "Er klasse I brugsgenstande tilsluttet til beskyttelseslederen?";
 
     //Måleresultater
     String text = "Måleresultater" +
@@ -116,15 +116,34 @@ public class VerificationChart {
         questions.add(10, question10);
         questions.add(11, question11);
         questions.add(12, question12);
-
-
-
+        questions.add(13, question13);
+        questions.add(14, question14);
+        questions.add(15, question15);
+        questions.add(16, question16);
+        questions.add(17, question17);
+        questions.add(18, question18);
+        questions.add(19, question19);
+        questions.add(20, question20);
+        questions.add(21, question21);
+        questions.add(22, question22);
+        questions.add(23, question23);
+        questions.add(24, question24);
+        questions.add(25, question25);
+        questions.add(26, question26);
+        questions.add(27, question27);
+        questions.add(28, question28);
+        questions.add(29, question29);
+        questions.add(30, question30);
+        questions.add(31, question31);
+        questions.add(32, question32);
+        questions.add(33, question33);
+        questions.add(34, question34);
+        questions.add(35, question35);
+        questions.add(36, question36);
+        questions.add(37, question37);
+        questions.add(38, question38);
 
     }
-
-
-
-
 
 
     public void setCostumerName(String costumerName) {
@@ -164,7 +183,6 @@ public class VerificationChart {
     }
 
 
-
     public String getCostumerName() {
         return costumerName;
     }
@@ -200,195 +218,5 @@ public class VerificationChart {
     public String getDate() {
         return Date;
     }
-
-    public String getText1() {
-        return text1;
-    }
-
-    public String getQuestion1_1() {
-        return question0;
-    }
-
-    public String getQuestion1() {
-        return question1;
-    }
-
-    public String getQuestion2() {
-        return question2;
-    }
-
-    public String getQuestion3() {
-        return question3;
-    }
-
-    public String getQuestion4() {
-        return question4;
-    }
-
-    public String getQuestion5() {
-        return question5;
-    }
-
-    public String getQuestion6() {
-        return question6;
-    }
-
-    public String getQuestion7() {
-        return question7;
-    }
-
-    public String getQuestion8() {
-        return question8;
-    }
-
-    public String getQuestion9() {
-        return question9;
-    }
-
-    public String getQuestion10() {
-        return question10;
-    }
-
-    public String getQuestion11() {
-        return question11;
-    }
-
-    public String getQuestion12() {
-        return question12;
-    }
-
-    public String getText2() {
-        return text2;
-    }
-
-    public String getGetQuestion13() {
-        return getQuestion13;
-    }
-
-    public String getGetQuestion14() {
-        return getQuestion14;
-    }
-
-    public String getGetQuestion15() {
-        return getQuestion15;
-    }
-
-    public String getGetQuestion16() {
-        return getQuestion16;
-    }
-
-    public String getGetQuestion17() {
-        return getQuestion17;
-    }
-
-    public String getGetQuestion18() {
-        return getQuestion18;
-    }
-
-    public String getGetQuestion19() {
-        return getQuestion19;
-    }
-
-    public String getGetQuestion20() {
-        return getQuestion20;
-    }
-
-
-    public String getText3() {
-        return text3;
-    }
-
-    public String getGetQuestion3_1() {
-        return getQuestion3_1;
-    }
-
-    public String getGetQuestion3_2() {
-        return getQuestion3_2;
-    }
-
-    public String getGetQuestion3_3() {
-        return getQuestion3_3;
-    }
-
-    public String getGetQuestion3_4() {
-        return getQuestion3_4;
-    }
-
-    public String getGetQuestion3_5() {
-        return getQuestion3_5;
-    }
-
-    public String getGetQuestion3_6() {
-        return getQuestion3_6;
-    }
-
-    public String getGetQuestion3_7() {
-        return getQuestion3_7;
-    }
-
-
-
-    public String getText4() {
-        return text4;
-    }
-
-    public String getGetQuestion4_1() {
-        return getQuestion4_1;
-    }
-
-    public String getGetQuestion4_2() {
-        return getQuestion4_2;
-    }
-
-
-    public String getText5() {
-        return text5;
-    }
-
-    public String getGetQuestion5_1() {
-        return getQuestion5_1;
-    }
-
-    public String getGetQuestion5_2() {
-        return getQuestion5_2;
-    }
-
-    public String getGetQuestion5_3() {
-        return getQuestion5_3;
-    }
-
-    public String getGetQuestion5_4() {
-        return getQuestion5_4;
-    }
-
-    public String getGetQuestion5_5() {
-        return getQuestion5_5;
-    }
-
-    public String getGetQuestion5_6() {
-        return getQuestion5_6;
-    }
-
-
-    public String getText6() {
-        return text6;
-    }
-
-    public String getGetQuestion6_1() {
-        return getQuestion6_1;
-    }
-
-    public String getGetQuestion6_2() {
-        return getQuestion6_2;
-    }
-
-    public String getGetQuestion6_3() {
-        return getQuestion6_3;
-    }
-
-
-    public String getText() {
-        return text;
-    }
-
 }
+
