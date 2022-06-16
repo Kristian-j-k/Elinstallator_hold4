966df8b841c707c1620dd79aae9e4b46867516f2
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2022-06-16 17:38:50 +0200

    DataBaseHelper med singelton og private constructor

--- app/src/main/java/com/elinstallator_hold4/DataBaseHelper.java
+++ app/src/main/java/com/elinstallator_hold4/DataBaseHelper.java
@@ -9,19 +9,38 @@ package com.elinstallator_hold4;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
+    private static DataBaseHelper mInstance = null;
+    private Context mCxt;
 
-    public DataBaseHelper(@Nullable Context context) {
-        super(context, "DbElinstallator.db", null, 1);
+
+    public static final String TABLE_NAME = "DbElinstallator20";
+    public static final String COL_1 = "user_id";
+
+
+
+    private DataBaseHelper(Context context) {
+        super(context, TABLE_NAME, null, 1);
+        this.mCxt = context;
     }
+
+    public static DataBaseHelper getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new DataBaseHelper(context.getApplicationContext());
+        }
+        return mInstance;
+    }
+
+
 
     @Override
     public  void onCreate(SQLiteDatabase db){
-        String createTableStatement = "CREATE TABLE USER (user_id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, pasword TEXT NOT NULL)";
+        String createTableStatement = "CREATE TABLE "+ TABLE_NAME +" (user_id INTEGER PRIMARY KEY)";
         db.execSQL(createTableStatement);
     }
     //if the version number change later on, then old users will be updated
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
     }
 }
